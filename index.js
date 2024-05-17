be5739d79a1faa39a1eae141a2f8af6355acba33
SivasankarH <155282396+SivasankarH@users.noreply.github.com>
2024-05-17 17:14:18 +0530

    Add basic CSS & HTML for Calculator

--- index.js
+++ index.js
@@ -37,3 +37,22 @@ console.log(operate(10,'sub',2))
 console.log(operate(10,'mul',2))
 console.log(operate(10,'div',2))
 
+const display = document.querySelector('display');
+const one = document.querySelector('one');
+const two = document.querySelector('two');
+const three = document.querySelector('three');
+const four = document.querySelector('four');
+const five = document.querySelector('five');
+const six = document.querySelector('six');
+const seven = document.querySelector('seven');
+const eight = document.querySelector('eight');
+const nine = document.querySelector('nine');
+const zero = document.querySelector('zero');
+const addition = document.querySelector('add');
+const subtraction = document.querySelector('sub');
+const multipy = document.querySelector('multi');
+const divison = document.querySelector('divide');
+
+
+
+
