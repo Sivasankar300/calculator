85c57900dce9eb6b5fb7fd47bf19324e478a4eb8
SivasankarH <155282396+SivasankarH@users.noreply.github.com>
2024-05-20 20:06:20 +0530

    Add EventListeners for each number key

--- index.js
+++ index.js
@@ -33,26 +33,104 @@ switch(op){
 
 }
 
-let calculator = document.querySelector('calculator');
-const display = document.querySelector('display');
-const one = document.querySelector('one');
-const two = document.querySelector('two');
-const three = document.querySelector('three');
-const four = document.querySelector('four');
-const five = document.querySelector('five');
-const six = document.querySelector('six');
-const seven = document.querySelector('seven');
-const eight = document.querySelector('eight');
-const nine = document.querySelector('nine');
-const zero = document.querySelector('zero');
-const addition = document.querySelector('add');
-const subtraction = document.querySelector('sub');
-const multipy = document.querySelector('multi');
-const divison = document.querySelector('divide');
+let calculator = document.querySelector('.calculator');
+const display = document.querySelector('.display');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+const addition = document.querySelector('.add');
+const subtraction = document.querySelector('.sub');
+const multipy = document.querySelector('.multi');
+const divison = document.querySelector('.divide');
 
 const keys = document.querySelectorAll('.keys')
-const fifthRow = document.querySelector('fifthRow')
+const fifthRow = document.querySelector('.fifthRow')
 
-let storedValue = [];
+/* How to listen to a elements (SOLVED)
+   How to display elements one over data and store them individually (SOLVED)
+   How to know which number to assign if all the listeners are grouped then
+   are executed based on which one was pressed (Unsolved) */
+
+   let storedValue = []
+   let displayValue;
+
+one.addEventListener("click", () => {
+    storedValue.push(1);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+two.addEventListener("click", () => {
+    storedValue.push(2);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+three.addEventListener("click", () => {
+    storedValue.push(3);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+
+four.addEventListener("click", () => {
+    storedValue.push(4);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+
+five.addEventListener("click", () => {
+    storedValue.push(5);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+
+six.addEventListener("click", () => {
+    storedValue.push(6);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+    console.log(storedValue)
+})
+
+
+seven.addEventListener("click", () => {
+    storedValue.push(7);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+
+eight.addEventListener("click", () => {
+    storedValue.push(8);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+
+nine.addEventListener("click", () => {
+    storedValue.push(9);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+
+zero.addEventListener("click", () => {
+    storedValue.push(0);
+    displayValue = storedValue.join('')
+    display.textContent = displayValue;
+})
+
+
+
+
 
 
