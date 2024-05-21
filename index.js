69cc1f3bf3b2353e284265d4e30e773f2efe2601
SivasankarH <155282396+SivasankarH@users.noreply.github.com>
2024-05-21 14:42:58 +0530

    Fix inconsistency with html classes for operators

--- index.js
+++ index.js
@@ -22,44 +22,51 @@ function operate(a,op,b){
 
 switch(op){
     case 'add':
-        return add(a,b)
+        return result = add(a,b)
     case 'sub':
-        return sub(a,b)
+        return result = sub(a,b)
     case 'mul':
-        return mul(a,b)
+        return result = mul(a,b)
     case 'div':
-        return div(a,b)
+        return result = div(a,b)
 }
 
 }
-console.log(operate(10,'add',2))
-console.log(operate(10,'sub',2))
-console.log(operate(10,'mul',2))
-console.log(operate(10,'div',2))
 
-const display = document.querySelector('display');
-const one = document.querySelector('one');
-const two = document.querySelector('two');
-const three = document.querySelector('three');
-const four = document.querySelector('four');
-const five = document.querySelector('five');
-const six = document.querySelector('six');
-const seven = document.querySelector('seven');
-const eight = document.querySelector('eight');
-const nine = document.querySelector('nine');
-const zero = document.querySelector('zero');
-const addition = document.querySelector('add');
-const subtraction = document.querySelector('sub');
-const multipy = document.querySelector('multi');
-const divison = document.querySelector('divide');
 
-/* How to listen to a elements (SOLVED)
-   How to display elements one over data and store them individually (SOLVED)
-   How to know which number to assign if all the listeners are grouped then
-   are executed based on which one was pressed (Unsolved) */
+const display = document.querySelector('.display');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+const addition = document.querySelector('.add');
+const subtraction = document.querySelector('.sub');
+const multiplication = document.querySelector('.mul');
+const divison = document.querySelector('.div');
+const clear = document.querySelector(".clear")
+const equal = document.querySelector(".equal")
+
+/* -How to listen to a elements (SOLVED)
+   -How to display elements one over data and store them individually (SOLVED)
+    -How to know which number to assign if all the listeners are grouped then
+    are executed based on which one was pressed (Decided to do it individually)
+    -How to erase the display value upon pressing clear (SOLVED
+    -How to make equal display the result of the operation on the display (SOLVED)
+    -How to store the first and second values in a way numbers larger than a single digit
+    can still work unlike using an array (UNSOLVED) */
 
    let storedValue = []
    let displayValue;
+   let operation;
+   let result;
+   let firstValue;
+   let secondValue
 
 one.addEventListener("click", () => {
     storedValue.push(1);
@@ -127,6 +134,41 @@ zero.addEventListener("click", () => {
     storedValue.push(0);
     displayValue = storedValue.join('')
     display.textContent = displayValue;
+})
+
+clear.addEventListener("click", () =>{
+    displayValue = " "
+    display.textContent = " "
+    storedValue = []
+})
+
+addition.addEventListener("click",()=> {
+    operation = 'add';
+    display.textContent = " ";
+
+})
+
+subtraction.addEventListener("click ",()=> {
+    operation = 'sub'
+    display.textContent = " "
+})
+
+multiplication.addEventListener("click ",()=> {
+    operation = 'mul'
+    display.textContent = " "
+})
+
+divison.addEventListener("click ",()=> {
+    operation = 'div'
+    display.textContent = " "
+    
+})
+
+equal.addEventListener("click",()=> {
+    let n = storedValue.length;
+    operate(parseInt(storedValue[n-2]),operation,parseInt(storedValue[n-1]))
+    display.textContent = result;
+    storedValue.push(result);
 })
 
 
